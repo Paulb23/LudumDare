74c8e4bead17af9ee83a61b7bee547306dfd590b
Crazy046 <crazy046@hotmail.co.uk>
2014-04-26 03:35:19 +0100

    Menu system

--- src/net/net23/paulb/ludumdare/states/Menu.java
+++ src/net/net23/paulb/ludumdare/states/Menu.java
@@ -2,6 +2,8 @@ package net.net23.paulb.ludumdare.states;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -9,6 +11,7 @@ import org.newdawn.slick.state.StateBasedGame;
 public class Menu extends BasicGameState {
 	
 	private int state;
+	private Image title;
 	
 	public Menu(int state) {
 		this.state = state;
@@ -16,16 +19,21 @@ public class Menu extends BasicGameState {
 	
 	@Override
 	public void init(GameContainer gc, StateBasedGame gs) throws SlickException {
-		
+		this.title = new Image("res/ui/title.png");
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame gs, Graphics g) throws SlickException {
-		
+		this.title.draw(50,50);
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame gs, int delta) throws SlickException {
+		Input input = gc.getInput();
+		
+		if (input.isKeyDown(Input.KEY_ENTER)) {
+			gs.enterState(1);
+		}
 	}
 
 	@Override
